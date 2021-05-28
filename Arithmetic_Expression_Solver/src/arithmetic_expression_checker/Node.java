4cab0fcb1fb331787b8f8d4ad970b83953f4a324
Md.Sabbir Ahmed <62795438+MSAhmed1414@users.noreply.github.com>
2021-05-28 23:48:32 +0600

    Add files via upload

---
+++ Arithmetic_Expression_Solver/src/arithmetic_expression_checker/Node.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arithmetic_expression_checker;
+
+/**
+ *
+ * @author M S A
+ */
+public class Node{
+  public Object val;
+  public Node next;
+  
+  public Node(Object v, Node n){
+    val =v ;
+    next = n;
+    
+  }
+  
+}
